f8a3ae813e394fd77f60f7de242367685d0a349e
Alex Chien <alex.chien@koocaa.com>
2013-09-06 01:54:35 +0800

    .js

--- lib/rails_admin_jcrop/engine.rb
+++ lib/rails_admin_jcrop/engine.rb
@@ -3,7 +3,12 @@ module RailsAdminJcrop
     isolate_namespace RailsAdmin
 
     initializer "RailsAdminJcrop precompile hook" do |app|
-      app.config.assets.precompile += ['rails_admin/ra.jcrop.js', 'rails_admin/jquery.Jcrop.js', 'rails_admin/jquery.Jcrop.css']
+      # app.config.assets.precompile += ['rails_admin/ra.jcrop.js', 'rails_admin/jquery.Jcrop.js', 'rails_admin/jquery.Jcrop.css']
+      app.config.assets.precompile += %w(
+        rails_admin/ra.jcrop.js
+        rails_admin/jquery.Jcrop.js
+        rails_admin/jquery.Jcrop.css
+      )
     end
   end
 end
